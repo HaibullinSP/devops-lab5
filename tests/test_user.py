cde120a3f723ea546d081cdf9d8690ed978d9bf2
HaibullinSP <sergeihaibullin2@gmail.com>
2026-05-08 13:55:36 +0300

    Изменение test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -34,24 +34,24 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    response = client.get("/api/v1/user", params={'email': users[3]['email']})
+    response = client.get("/api/v1/user", params={'email': users[2]['email']})
     assert response.status_code == 404
     assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
     response = client.post("/api/v1/user", json={
-        'name': users[3]['name'],
-        'email': users[3]['email']
+        'name': users[2]['name'],
+        'email': users[2]['email']
     })
     assert response.status_code == 201
     user_id = response.json()
     assert isinstance(user_id, int)
    
-    get_response = client.get("/api/v1/user", params={'email': users[3]['email']})
+    get_response = client.get("/api/v1/user", params={'email': users[2]['email']})
     assert get_response.status_code == 200
-    assert get_response.json()['name'] == users[3]['name']
-    assert get_response.json()['email'] == users[3]['email']
+    assert get_response.json()['name'] == users[2]['name']
+    assert get_response.json()['email'] == users[2]['email']
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
@@ -64,10 +64,10 @@ def test_create_user_with_invalid_email():
 
 def test_delete_user():
     '''Удаление пользователя'''
-    delete_response = client.delete("/api/v1/user", params={'email': users[2]['email']})
+    delete_response = client.delete("/api/v1/user", params={'email': users[1]['email']})
     assert delete_response.status_code == 204
     assert delete_response.text == ''
     
-    get_response = client.get("/api/v1/user", params={'email': users[2]['email']})
+    get_response = client.get("/api/v1/user", params={'email': users[1]['email']})
     assert get_response.status_code == 404
     assert get_response.json() == {"detail": "User not found"}
